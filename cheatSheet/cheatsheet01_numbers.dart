aa1797ea2a0550d76ea303e0defc2aee2dfc9520
hardardottir93 <hafrunhardar@gmail.com>
2025-02-17 20:48:16 +0000

    uppfært

--- cheatSheet/cheatsheet01_numbers.dart
+++ cheatSheet/cheatsheet01_numbers.dart
@@ -41,5 +41,39 @@ void main(List<String> arguments){
   username = username.toLowerCase();
   print(username);
   String emailAddress = username + "@" + url;
-  print(emailAddress);
+
+  print(removeIcelandicLetters(emailAddress));
+}
+
+
+
+String removeIcelandicLetters(String input) {
+  Map<String, String> icelandicToLatin = {
+    'ð': 'd',
+    'þ': 'th',
+    'á': 'a',
+    'é': 'e',
+    'í': 'i',
+    'ó': 'o',
+    'ú': 'u',
+    'ý': 'y',
+    'ö': 'o',
+    'Æ': 'Ae',
+    'æ': 'ae',
+    'Ð': 'D',
+    'Þ': 'Th',
+    'Á': 'A',
+    'É': 'E',
+    'Í': 'I',
+    'Ó': 'O',
+    'Ú': 'U',
+    'Ý': 'Y',
+    'Ö': 'O'
+  };
+
+  icelandicToLatin.forEach((key, value) {
+    input = input.replaceAll(key, value);
+  });
+
+  return input;
 }
--- cheatSheet/class01.dart
+++ cheatSheet/class01.dart
@@ -1,4 +1,7 @@
 import 'package:ntv_vor_25/methods.dart';
+import 'dart:convert';
+import 'dart:io';
+
 void main(List<String> arguments) {
   /*
     0. Create a personal message. Use a variable to represent a person's name, and print a
@@ -7,6 +10,10 @@ void main(List<String> arguments) {
     Store Message and Name in seperate variable.
    */
 
+  print("Hvað heitirðu?");
+  String name = stdin.readLineSync(encoding: utf8).toString();
+  print("Góðan daginn $name, ertu til í að læra Dart með mér í dag?");
+
   /* 1. Find a quote from a famous person you admire. Print the quote and the name of its
   author. Your output should look something like the following, including the
   quotation marks:
@@ -14,15 +21,28 @@ void main(List<String> arguments) {
   anything new.'
   */
 
+  String author = "Albert Einstein";
+  String quote = "Life is like riding a bicycle. To keep your balance, you must keep moving.";
+  print("$author sagði eitt sinn '$quote'");
+
   /*
     2. Assign a message to a variable, and print that message. Then change the value of
   the variable to a new message, and print the new message.
    */
 
+  String message = "Velkomin!";
+  print(message);
+  message = message.replaceAll(message, "Bless!");
+  print(message);
+
   /*
     3.  Remove all the spaces from the rhyme. And print the modified rhyme.
    */
+
   String NureseryRyhme = "Eena, meena, mina, mo, Catch a mouse by the toe; If he squeals let him go, Eeena, meena, mina, mo.";
+  NureseryRyhme = NureseryRyhme.replaceAll(" ", "");
+  print(NureseryRyhme);
+
   /*
    4. Prompt a user for their full name (first name and last name)
     1. Store the users entry.
@@ -31,6 +51,15 @@ void main(List<String> arguments) {
     4. Capitalise and display the users full name with each word having a capital
     first letter, and the other letters being lowercase.
    */
+
+  print("Hvað heitirðu fullu nafni?");
+  String fullName = stdin.readLineSync(encoding: utf8).toString();
+  fullName = fullName.toUpperCase();
+  print(fullName);
+
+  List<String> names = fullName.split(" ");
+  names[0] = names[0].replaceAll(names[0], "Hafrún");
+  print(names.toString());
 
   /*
     5. Display the following SSN on the correct Format:
