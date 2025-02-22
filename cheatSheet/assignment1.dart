93afc5400d82956d2786b1e4417ab8b7242edf37
hardardottir93 <hafrunhardar@gmail.com>
2025-02-22 22:25:32 +0000

    Create assignment1.dart

---
+++ cheatSheet/assignment1.dart
@@ -0,0 +1,26 @@
+void main() {
+  // 1.
+  print('Hello \nHafrún');
+
+  //2.
+  int sum = 2+2;
+  print(sum);
+
+  //3.
+  double div = 4/2;
+  print(div);
+
+  //4.
+  int result1 = -1+5*5;
+  print(result1);
+
+  int result2 = (25+5)%4;
+  print(result2);
+
+  double result3 = 8+6*4/2;
+  print(result3.toInt());
+
+  double result4 = 2+15/6*1-7%2;
+  print(result4);
+}
+
