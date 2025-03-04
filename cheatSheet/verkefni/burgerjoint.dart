436064a52e7459dd7451645dc6a09a8d9c3b29c8
hardardottir93 <hafrunhardar@gmail.com>
2025-03-04 07:50:16 +0000

    Update burgerjoint.dart

--- cheatSheet/verkefni/burgerjoint.dart
+++ cheatSheet/verkefni/burgerjoint.dart
@@ -10,7 +10,7 @@ void main() {
   for (int i = 0 ; i < menu.length ; i++) {
       print('${'${i+1}. ${menu[i][0]}'} - ${menu[i][1]} kr.');
   }
-
+String addOrRemove = '\n Skrifaðu "add" ef þú vilt bæta við pöntun og "remove" ef þú vilt eyða úr pöntun.';
   String orderText = '\nVinsamlegast veldu númer á réttinu sem þú vilt bæta við pöntunina (eða skrifaðu "exit" til að hætta!)';
   print(orderText);
 
@@ -22,31 +22,33 @@ void main() {
     String? input = stdin.readLineSync();
     int? inputInt = int.tryParse(input!);
     int? sum = 0;
-    if (input == 'exit') {
-      print('Þakka þér fyrir að panta! Hér er pöntunin þín:');
-      for (int i = 0 ; i < order.length ; i++) {
-        sum = (sum! + (order[i][1])) as int?;
-        print('${'${i+1}. ${order[i][0]}'} - ${order[i][1]} kr.');
-      }
-      print('Samtals: $sum kr.');
-      ordering = false;
-    }
-    else if (inputInt == null) {
-        print('Vinsamlegast veldu númer á matseðlinum eða skrifaðu exit til að hætta.');
-    }
-    else if (inputInt > 0 && inputInt <= menu.length ){
-      print(menu[inputInt-1][0] + ' hefur verið bætt við pöntunina þína.\n');
-      order.add(menu[inputInt-1]);
 
-      print('Þín pöntun:');
-      for (int i = 0 ; i < order.length ; i++) {
-        sum = (sum! + (order[i][1])) as int?;
-        print('${'${i+1}. ${order[i][0]}'} - ${order[i][1]} kr.');
+      if (input == 'exit') {
+        print('Þakka þér fyrir að panta! Hér er pöntunin þín:');
+        for (int i = 0; i < order.length; i++) {
+          sum = (sum! + (order[i][1])) as int?;
+          print('${'${i + 1}. ${order[i][0]}'} - ${order[i][1]} kr.');
+        }
+        print('Samtals: $sum kr.');
+        ordering = false;
       }
+      else if (inputInt == null) {
+        print(orderText);
+      }
+      else if (inputInt > 0 && inputInt <= menu.length) {
+        print(
+            menu[inputInt - 1][0] + ' hefur verið bætt við pöntunina þína.\n');
+        order.add(menu[inputInt - 1]);
 
-      print('Samtals: $sum kr.');
-    } else {
-      print('Vinsamlegast veldu númer á matseðlinum eða skrifaðu exit til að hætta.');
+        print('Þín pöntun:');
+        for (int i = 0; i < order.length; i++) {
+          sum = (sum! + (order[i][1])) as int?;
+          print('${'${i + 1}. ${order[i][0]}'} - ${order[i][1]} kr.');
+        }
+
+        print('Samtals: $sum kr.');
+      } else {
+        print(orderText);
+      }
     }
-  }
 }
